eec550788eb4663f98ea1dee2834c1f61b6b584d
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-01-27 19:44:55 +0100

    add rest doc for qlaffont/fortnite-api@2.5.0

--- api.js
+++ api.js
@@ -239,6 +239,17 @@ app.get('/v1/store', function(req, res) {
     });
 })
 
+// swagger file
+app.get('/swagger.json', function(req, res){
+  var file = __dirname + '/swagger.json';
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/swagger.yaml', function(req, res){
+  var file = __dirname + '/swagger.yaml';
+  res.download(file); // Set disposition and send it.
+});
+
 
 //swaggerUi
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
