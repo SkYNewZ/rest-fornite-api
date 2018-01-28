4c3f10bfad7018bd30dbfb9b8031df0242fbf01f
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-01-28 16:57:51 +0100

    change routes logic

--- api.js
+++ api.js
@@ -1,31 +1,17 @@
-var Fortnite = require('fortnite-api'),
-  express = require('express'),
+var express = require('express'),
   app = express(),
   morgan = require('morgan'),
   swaggerUi = require('swagger-ui-express'),
-  swaggerDocument = require('./swagger.json'),
   auth = [];
 
-if (process.env.LOGIN_EMAIL) {
-  auth.push(process.env.LOGIN_EMAIL);
-}
+//routes methods
+var user = require('./routes/user'),
+  news = require('./routes/news'),
+  pve = require('./routes/pve'),
+  stats = require('./routes/stats'),
+  store = require('./routes/store'),
+  check = require('./routes/check');
 
-if (process.env.LOGIN_PASSWORD) {
-  auth.push(process.env.LOGIN_PASSWORD);
-}
-
-if (process.env.OAUTH_EPIC_LAUNCHER) {
-  auth.push(process.env.OAUTH_EPIC_LAUNCHER);
-}
-
-if (process.env.OAUTH_FORTNITE) {
-  auth.push(process.env.OAUTH_FORTNITE);
-}
-
-// Authentification
-var fortniteAPI = new Fortnite(auth);
-
-//app configuration
 //don't show the log when it is test
 if (process.env.NODE_ENV !== 'test') {
   //use morgan to log at command line
@@ -37,223 +23,40 @@ app.all('/*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+app.use('/static', express.static('public'));
 
-// get user by name
-app.get('/v1/user/:platform/:username', function(req, res) {
-  var username = req.params.username;
-  var platform = req.params.platform;
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.checkPlayer(username, platform)
-        .then((stats) => {
-          res.json(stats);
-        })
-        .catch((err) => {
-          if (err === "Player Not Found") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else if (err === "Impossible to fetch User. User not found on this platform") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else {
-            res.status(500).send({
-              code: 500,
-              message: err
-            });
-          }
-        });
-    });
-});
+// check user bu username
+app.route('/v1/user/:platform/:username').get(user.checkPlayer);
 
-// get user stats by name
-app.get('/v1/stats/:platform/:username', function(req, res) {
-  var username = req.params.username;
-  var platform = req.params.platform;
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.getStatsBR(username, platform)
-        .then((stats) => {
-          res.json(stats);
-        })
-        .catch((err) => {
-          if (err === "Player Not Found") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else if (err === "Impossible to fetch User. User not found on this platform") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else {
-            res.status(500).send({
-              code: 500,
-              message: err
-            });
-          }
-        });
-    });
-});
+// get user stats by username
+app.route('/v1/stats/:platform/:username').get(stats.getStatsBR);
 
 // get users stats by user id
-app.get('/v1/stats/id/:platform/:id', function(req, res) {
-  var id = req.params.id;
-  var platform = req.params.platform;
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.getStatsBRFromID(id, platform)
-        .then((stats) => {
-          res.json(stats);
-        })
-        .catch((err) => {
-          if (err === "Impossible to fetch User. User not found on this platform") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else if (err === "Impossible to fetch User.") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else {
-            res.status(500).send({
-              code: 500,
-              message: err
-            });
-          }
-        });
-    });
-})
+app.route('/v1/stats/id/:platform/:id').get(stats.getStatsBRFromID);
 
-// PVE stats
-app.get('/v1/pve/:username', function(req, res) {
-  var username = req.params.username;
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.getStatsPVE(username)
-        .then((stats) => {
-          res.json(stats);
-        })
-        .catch((err) => {
-          if (err === "Player Not Found") {
-            res.status(404).send({
-              code: 404,
-              message: err
-            });
-          } else if (err === "No Data") {
-            res.status(400).send({
-              code: 400,
-              message: err
-            });
-          } else {
-            res.status(500).send({
-              code: 500,
-              message: err
-            });
-          }
-        });
-    });
-})
-
-// get fortnite news
-app.get('/v1/news/:lang?', function(req, res) {
-  var language = req.params.lang || 'en';
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.getFortniteNews(language)
-        .then((news) => {
-          res.json(news);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            code: 500,
-            message: err
-          });
-        });
-    });
-})
-
-// get fortnite status
-app.get('/v1/check', function(req, res) {
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.checkFortniteStatus()
-        .then((status) => {
-          res.json(status);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            code: 500,
-            message: err
-          });
-        });
-    });
-})
-
-// get store
-app.get('/v1/store/:lang?', function(req, res) {
-  var language = req.params.lang || 'en';
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.getStore(language)
-        .then((store) => {
-          res.json(store);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            code: 500,
-            message: err
-          });
-        });
-    });
-})
+// PVE stats by username
+app.route('/v1/pve/:username').get(pve.getStatsPVE);
 
 // getFortnitePVEInfo
-app.get('/v1/pve/info/:lang?', function(req, res) {
-  var language = req.params.lang || 'en';
-  fortniteAPI.login()
-    .then(() => {
-      fortniteAPI.getFortnitePVEInfo(language)
-        .then((store) => {
-          res.json(store);
-        })
-        .catch((err) => {
-          res.status(500).send({
-            code: 500,
-            message: err
-          });
-        });
-    });
-})
+app.route('/v1/pve/info/:lang?').get(pve.getFortnitePVEInfo);
 
-// swagger file
-app.get('/v1/swagger.json', function(req, res) {
-  var file = __dirname + '/swagger.json';
-  res.download(file); // Set disposition and send it.
-});
+// get fortnite news
+app.route('/v1/news/:lang?').get(news.getFortniteNews);
 
-app.get('/v1/swagger.yaml', function(req, res) {
-  var file = __dirname + '/swagger.yaml';
-  res.download(file); // Set disposition and send it.
-});
+// get fortnite status
+app.route('/v1/check').get(check.checkFortniteStatus);
+
+// get store
+app.route('/v1/store/:lang?').get(store.getStore);
 
 
 //swaggerUi
+var swaggerDocument = require('./public/swagger.json');
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // start server
 app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'))
-});
-
-process.on('SIGINT', function() {
-  process.exit();
 });
 
 module.exports = app;
