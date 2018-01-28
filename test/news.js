a6e787c7fe3ca68a97f4e5e7af18df1c2cf42208
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-01-28 15:20:46 +0100

    add news in french

--- test/news.js
+++ test/news.js
@@ -11,10 +11,23 @@ chai.use(chaiHttp);
 
 //Our parent block
 describe('News', () => {
-  describe('/GET /v1/news', () => {
+  describe('/GET /v1/news/:lang?', () => {
     it('it should return news', (done) => {
       chai.request(server)
         .get('/v1/news')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        });
+
+    });
+  });
+
+  describe('/GET /v1/news/:lang?', () => {
+    it('it should return news in french', (done) => {
+      chai.request(server)
+        .get('/v1/news/fr')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
