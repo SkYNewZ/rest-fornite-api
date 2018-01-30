3fea65263611f3b7470880cd928d79ea8c991aae
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-01-30 09:51:43 +0100

    change store logic for only get BRDailyStorefront & BRWeeklyStorefront

--- src/store.js
+++ src/store.js
@@ -2,21 +2,26 @@ module.exports = {
   // resize store object and add picture path
   convert: (store, url) => {
     return new Promise((resolve) => {
-
+      var newStoreFront = [];
       store.storefronts.forEach((storefront) => {
-        // change only the BRDailyStorefront and BRWeeklyStorefront
+        // keep only the BRDailyStorefront and BRWeeklyStorefront
         if (storefront.name === "BRDailyStorefront" || storefront.name === "BRWeeklyStorefront") {
-          var catalogEntries = storefront.catalogEntries;
-          catalogEntries.forEach((catalogEntry, index) => {
-            catalogEntries[index] = {
+          var newCatalogEntries = [];
+          storefront.catalogEntries.forEach((catalogEntry, index) => {
+            newCatalogEntries.push({
               devName: catalogEntry.devName,
               offerId: catalogEntry.offerId,
               prices: catalogEntry.prices,
               picturePath: url + "/static/store/" + catalogEntry.devName.replace(/ /g, "_") + ".png"
-            }
-          })
+            })
+          });
+          newStoreFront.push({
+            name: storefront.name,
+            catalogEntries: newCatalogEntries
+          });
         }
       });
+      store.storefronts = newStoreFront;
       resolve(store);
     });
   }
