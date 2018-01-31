75863bef3b2aa6bf5b9a59c42d687cc2b22b30b2
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-01-31 21:39:10 +0100

    fix path

--- test/assets.js
+++ test/assets.js
@@ -12,10 +12,10 @@ chai.use(chaiHttp);
 
 //Our parent block
 describe('Static files, test only one', () => {
-  describe('/GET ' + Config.static_uri + ' /store/[VIRTUAL]1_x_Candy_Axe_for_1500_MtxCurrency.png', () => {
+  describe('/GET ' + Config.static_uri + '/store/[VIRTUAL]1_x_Candy_Axe_for_1500_MtxCurrency.png', () => {
     it('it should return fornite ETA', (done) => {
       chai.request(server)
-        .get(Config.static_uri + ' /store/[VIRTUAL]1_x_Candy_Axe_for_1500_MtxCurrency.png')
+        .get(Config.static_uri + '/store/[VIRTUAL]1_x_Candy_Axe_for_1500_MtxCurrency.png')
         .end((err, res) => {
           res.should.have.status(200);
           done();
