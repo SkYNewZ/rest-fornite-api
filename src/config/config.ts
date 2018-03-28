a3b4b7a7104c5af9b613a7976c589c595ac0a3e5
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-03-28 23:20:26 +0200

    fix redis expiration globally

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,12 @@ export const AppConfig = {
     port: process.env.REDIS_PORT || 6379,
     auth_pass: process.env.REDIS_PASSWORD || '',
     prefix: process.env.REDIS_PREFIX || 'api-fortnite',
-    expire: 21600
+    expire: {
+      "200": 3600,
+      "4xx": 10,
+      "5xx": 10,
+      "xxx": 1
+    }
   },
   static_uri: '/static',
   universal_analytics_id: 'UA-93941787-3',
--- src/index.ts
+++ src/index.ts
@@ -45,7 +45,6 @@ if (AppConfig.redis.host) {
 if (process.env.NODE_ENV !== 'test') {
   // use morgan to log at command line
   app.use(morgan('combined')) // 'combined' outputs the Apache style LOGs
-
   if (cacheClient) {
     // redis logs
     cacheClient.on('message', function (message: any) {
@@ -57,77 +56,35 @@ if (process.env.NODE_ENV !== 'test') {
 
 // <----ROUTING---->
 // check user bu username
-app.route('/user/:platform/:username')
-  .get(user.checkPlayer)
+app.get('/user/:platform/:username', user.checkPlayer)
 
 // get user stats by username
-app.route('/stats/:platform/:username')
-  .get(
-    // enable caching if cache enable ^^
-    function (req: express.Request, res: express.Response, next: express.NextFunction) {
-      if (cacheClient) {
-        cacheClient.route({
-          expire: 3600
-        })
-      }
-      next()
-    },
-    stats.getStatsBR)
+app.get('/stats/:platform/:username', stats.getStatsBR)
 
 // get users stats by user id
-app.route('/stats/id/:platform/:id')
-  .get(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    // enable caching if cache enable ^^
-    if (cacheClient) {
-      cacheClient.route({
-        expire: 3600
-      })
-    }
-    next()
-  }, stats.getStatsBRFromID)
+app.get('/stats/id/:platform/:id', stats.getStatsBRFromID)
 
 // PVE stats by username
-app.route('/pve/:username')
-  .get(pve.getStatsPVE)
+app.get('/pve/:username', pve.getStatsPVE)
 
 // getFortnitePVEInfo
-app.route('/pve/info/:lang?')
-  .get(pve.getFortnitePVEInfo)
+app.get('/pve/info/:lang?', pve.getFortnitePVEInfo)
 
 // get fortnite news
-app.route('/news/:lang?')
-  .get(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    // enable caching if cache enable ^^
-    if (cacheClient) {
-      cacheClient.route({
-        expire: 3600
-      })
-    }
-    next()
-  }, news.getFortniteNews)
+app.get('/news/:lang?', news.getFortniteNews)
 
 // get fortnite status
-app.route('/check')
-  .get(check.checkFortniteStatus)
+app.get('/check', check.checkFortniteStatus)
 
 // get store
-app.route('/store/:lang?')
-  .get(function (req: express.Request, res: express.Response, next: express.NextFunction) {
-    // enable caching if cache enable ^^
-    if (cacheClient) {
-      cacheClient.route({
-        expire: 3600
-      })
-    }
-    next()
-  }, store.getStore)
+app.get('/store/:lang?', store.getStore)
 
 // swaggerUi
 const swaggerDocument = require('./swagger/swagger.json')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 //display https://skynewz.github.io/rest-fornite-api/ on hone page
-app.get('/', function (rreq: express.Request, res: express.Response) {
+app.get('/', function (req: express.Request, res: express.Response) {
   res.send('<iframe src="https://skynewz.github.io/rest-fornite-api" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">Your browser doesn\'t support iframes</iframe>');
 });
 // The 404 Route (ALWAYS Keep this as the last route)
