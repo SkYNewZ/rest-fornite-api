fb88417cd0017827c3ac89b9922cd3e8421b55e4
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-03-28 20:21:48 +0200

    add documentation on homepage

--- src/index.ts
+++ src/index.ts
@@ -127,8 +127,8 @@ const swaggerDocument = require('./swagger/swagger.json')
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 //display https://skynewz.github.io/rest-fornite-api/ on hone page
-app.get('/', function(rreq: express.Request, res: express.Response) {
-  res.send('<iframe src="https://skynewz.github.io/rest-fornite-api/" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">Your browser doesn\'t support iframes</iframe>');
+app.get('/', function (rreq: express.Request, res: express.Response) {
+  res.send('<iframe src="https://skynewz.github.io/rest-fornite-api" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">Your browser doesn\'t support iframes</iframe>');
 });
 // The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', function (req: express.Request, res: express.Response) {
