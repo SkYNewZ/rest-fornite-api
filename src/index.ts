5f064001d0bf7fd78c5ecf2030fd92b31cbbdffa
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-04-02 22:49:00 +0200

    add midleware for oauth, and only for /api routes

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,26 @@
 // <----REQUIRED PACKAGES---->
 import { config } from "dotenv";
 config();
+import { json, urlencoded } from "body-parser";
 import * as express from "express";
 import * as cache from "express-redis-cache";
 import * as morgan from "morgan";
 import * as swaggerUi from "swagger-ui-express";
 import { AppConfig } from "./config/config";
-import { json } from "body-parser"
 
 const app: express.Application = express();
+const apiRoutes = express.Router();
+const freeRoutes = express.Router();
 
 // routes methods
 import * as check from "./routes/check";
 import * as news from "./routes/news";
 import * as pve from "./routes/pve";
+import * as security from "./routes/security";
 import * as stats from "./routes/stats";
 import * as store from "./routes/store";
 import * as user from "./routes/user";
 // <----END REQUIRED PACKAGES---->
-
-// <----APP CONFIG---->
-app.set("port", process.env.PORT || 3000);
-app.all(
-  "/*",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-  }
-);
-app.use(AppConfig.static_uri, express.static(__dirname + "/public"));
-app.use(json());
-// <----END APP CONFIG---->
 
 // <----REDIS ACTIVATION---->
 // enable redis if process.env.REDIS_HOST provided
@@ -38,6 +28,9 @@ let cacheClient: any = null;
 if (AppConfig.redis.host) {
   cacheClient = cache(AppConfig.redis);
   app.use(cacheClient.route());
+  cacheClient.on("message", (message: any) => {
+    console.log("[REDIS] : " + message);
+  });
 }
 // <----END REDIS ACTIVATION---->
 
@@ -45,62 +38,73 @@ if (AppConfig.redis.host) {
 /* istanbul ignore if */
 if (process.env.NODE_ENV !== "test") {
   app.use(morgan("combined"));
-  if (cacheClient) {
-    // redis logs
-    cacheClient.on("message", (message: any) => {
-      console.log("[REDIS] : " + message);
-    });
-  }
 }
 // <---END IF TESTING---->
 
-// <----ROUTING---->
-// check user bu username
-app.get("/user/:platform/:username", user.checkPlayer);
-
-// get user stats by username
-app.get("/stats/:platform/:username", stats.getStatsBR);
-
-// get users stats by user id
-app.get("/stats/id/:platform/:id", stats.getStatsBRFromID);
-
-// PVE stats by username
-app.get("/pve/user/:username", pve.getStatsPVE);
-
-// getFortnitePVEInfo
-app.get("/pve/info/:lang?", pve.getFortnitePVEInfo);
-
-// get fortnite news
-app.get("/news/:lang?", news.getFortniteNews);
-
-// get fortnite status
-app.get("/check", check.checkFortniteStatus);
-
-// get store
-app.get("/store/:lang?", store.getStore);
-
+// <----APP CONFIG---->
+app.set("port", process.env.PORT || 3000);
+app.use(AppConfig.static_uri, express.static(__dirname + "/public"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 // swaggerUi
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(null, {
-    swaggerUrl: "https://skynewz-api-fortnite.herokuapp.com/static/swagger.json"
-  })
+    swaggerUrl: "https://skynewz-api-fortnite.herokuapp.com/static/swagger.json",
+  }),
 );
 
+// oauth
+apiRoutes.use(security.authMiddleware);
+
+// set prefix
+app.use("/api", apiRoutes);
+app.use("/", freeRoutes);
+// <----END APP CONFIG---->
+
+// <----ROUTING---->
+
+// get token
+apiRoutes.post("/oauth/token", security.getToken);
+
+// check user bu username
+apiRoutes.get("/user/:platform/:username", user.checkPlayer);
+
+// get user stats by username
+apiRoutes.get("/stats/:platform/:username", stats.getStatsBR);
+
+// get users stats by user id
+apiRoutes.get("/stats/id/:platform/:id", stats.getStatsBRFromID);
+
+// PVE stats by username
+apiRoutes.get("/pve/user/:username", pve.getStatsPVE);
+
+// getFortnitePVEInfo
+apiRoutes.get("/pve/info/:lang?", pve.getFortnitePVEInfo);
+
+// get fortnite news
+apiRoutes.get("/news/:lang?", news.getFortniteNews);
+
+// get fortnite status
+apiRoutes.get("/check", check.checkFortniteStatus);
+
+// get store
+apiRoutes.get("/store/:lang?", store.getStore);
+
 // display https://skynewz.github.io/rest-fornite-api/ on home page
-app.get("/", (req: express.Request, res: express.Response) => {
+freeRoutes.get("/", (req: express.Request, res: express.Response) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
 // The 404 Route (ALWAYS Keep this as the last route)
-app.get("*", (req: express.Request, res: express.Response) => {
+freeRoutes.get("*", (req: express.Request, res: express.Response) => {
   res.sendStatus(404);
 });
-// <----END ROUTING---->
 
 // start server
 app.listen(app.get("port"), () =>
-  console.log("Listening on port " + app.get("port"))
+  console.log("Listening on port " + app.get("port")),
 );
 
 export const AppServer: express.Application = app;
