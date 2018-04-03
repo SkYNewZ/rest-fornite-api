5114083551c136df13dab135bf58435f15421b0b
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-04-03 11:31:36 +0200

    Check database connection before login

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,16 @@
+import { Client } from "pg";
+
+const client = new Client({
+  connectionString: process.env.DATABSE_CONNECTION_STING,
+});
+client.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log("Database connected");
+  }
+});
+
 export const AppConfig = {
   debug: process.env.DEBUG || false,
   secret: process.env.SECRET || "g6ePcR'G36]l63n",
