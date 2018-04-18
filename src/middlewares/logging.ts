2d95cf2bbf6af0c641ee46cd9d16510a7e6f259e
Quentin Lemaire <quentin.lemaire10@free.fr>
2018-04-18 04:09:35 +0200

    adding try catch on insert

--- src/middlewares/logging.ts
+++ src/middlewares/logging.ts
@@ -10,14 +10,19 @@ export function ApiLogger(
   parameters: object = null,
   userAgent: string = null,
 ) {
-  DatabaseClient.query(
-    "INSERT INTO logs(id, message, created_at, user_id, parameters, user_agent) VALUES(nextval('logs_id_seq'), $1, $2, $3, $4, $5)",
-    [message, new Date(), token.user_id, parameters, userAgent],
-    (err: Error, result: QueryResult) => {
-      /* istanbul ignore if */
-      if (err) {
-        throw err;
-      }
-    },
-  );
+
+  try {
+    DatabaseClient.query(
+      "INSERT INTO logs(id, message, created_at, user_id, parameters, user_agent) VALUES(nextval('logs_id_seq'), $1, $2, $3, $4, $5)",
+      [message, new Date(), token.user_id, parameters, userAgent],
+      (err: Error, result: QueryResult) => {
+        /* istanbul ignore if */
+        if (err) {
+          throw err;
+        }
+      },
+    );
+  } catch (error) {
+    console.error(error);
+  }
 }
